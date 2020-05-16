47ed0e6ba88581f68f8244261af0cf02c927743d
Bart Webber <bartacus.webber@gmail.com>
2020-05-16 09:42:00 -0700

    created add widget form to home page

--- widgetcollector/main_app/urls.py
+++ widgetcollector/main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add', views.WidgetCreate.as_view(), name='widget_create'),
 
 ]
--- widgetcollector/main_app/views.py
+++ widgetcollector/main_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Widget
 
 
 
 # Define the home view
 def home(request):
   return render(request, 'home.html')
+
+
+class WidgetCreate(CreateView):
+  model = Widget
+  fields = '__all__'
+  success_url = '/'  
